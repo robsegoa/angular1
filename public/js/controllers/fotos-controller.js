c68416c61db7b8a96eb65e09f011950b449f3870
Robson Michkinis Segoa <robson.segoa@neoway.com.br>
2017-02-14 18:30:27 -0200

    3 finalizado e com URI

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,16 +1,22 @@
-angular.module('alurapic').controller('FotosController', function($scope){
-	$scope.fotos = [
-		{
-			titulo : 'Leão',
-			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-		},
-		{
-			titulo : 'Leão 2',
-			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-		},
-		{
-			titulo : 'Leão 3',
-			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-		},
-	];
+angular.module('alurapic').controller('FotosController', function($scope, $http){
+	$scope.fotos = [];
+
+	/*	var promise = $http.get('v1/fotos');
+	
+	promise.then(function(retorno){
+		$scope.fotos = retorno.data;
+	}).catch(function(error){
+		console.log(error);
+	});
+*/	
+
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	}).error(function(erro){
+		console.log(erro)
+	});
+
+
+	
 });
