50e68331dd622217a9911f0da0f50c73aa4c0022
Robson Michkinis Segoa <robson.segoa@neoway.com.br>
2017-02-14 17:27:51 -0200

    Fotos Leão, chamada básica Angular

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,16 @@
 angular.module('alurapic').controller('FotosController', function($scope){
-	$scope.foto = {
-		titulo : 'Leao',
-		url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	};
+	$scope.fotos = [
+		{
+			titulo : 'Leão',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+		},
+		{
+			titulo : 'Leão 2',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+		},
+		{
+			titulo : 'Leão 3',
+			url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+		},
+	];
 });
